66e267c537cdd73571eebc63a5841b36dcf29fc3
William Wyatt <tsangares@gmail.com>
2022-11-29 17:30:19 -0800

    auto-update

--- source/intel.js
+++ source/intel.js
@@ -1,4 +1,9 @@
-const sat_version = "2.17"
+const sat_version = "2.18"
+
+/*! js-cookie v3.0.1 | MIT */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,o=e.Cookies=t();o.noConflict=function(){return e.Cookies=n,o}}())}(this,(function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)e[o]=n[o]}return e}return function t(n,o){function r(t,r,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n.write(r,t)+c}}return Object.create({set:r,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var t=document.cookie?document.cookie.split("; "):[],o={},r=0;r<t.length;r++){var i=t[r].split("="),c=i.slice(1).join("=");try{var u=decodeURIComponent(i[0]);if(o[u]=n.read(c,u),e===u)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){r(t,"",e({},n,{expires:-1}))},withAttributes:function(n){return t(this.converter,e({},this.attributes,n))},withConverter:function(n){return t(e({},this.converter,n),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(n)}})}({read:function(e){return'"'===e[0]&&(e=e.slice(1,-1)),e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}},{path:"/"})}));
+
+
 //Custom UI ComponentsNe
 const PlayerNameIconRowLink = (player) => {
 	let playerNameIconRow = Crux.Widget("rel col_black clickable")
@@ -398,6 +403,7 @@ setTimeout(() => {
 	*/
 }, 1000);
 
+
 function NeptunesPrideAgent() {
 	let title = (document && document.currentScript && document.currentScript.title) || `SAT ${sat_version}`;
 	let version = title.replace(/^.*v/, 'v');
@@ -420,7 +426,6 @@ function NeptunesPrideAgent() {
 		hotkeys.push([key, action]);
 		Mousetrap.bind(key, copy(action));
 	}
-
 
 	if (!String.prototype.format) {
 		String.prototype.format = function () {
@@ -1022,6 +1027,8 @@ function NeptunesPrideAgent() {
 					s = s.replace(pattern, apiLink);
 				} else if (sub.startsWith("data:")) {
 					s = s.replace(pattern, '<div width="100%" class="screenshot"><img class="screenshot" src="' + sub + '"/></div>');
+				} else if (sub.startsWith("https:&#x2F;&#x2F;i.imgur.com")) {
+					s = s.replace(pattern,`<img style="width:100%;" src='${sub}' />`)
 				} else {
 					s = s.replace(pattern, "(" + sub + ")");
 				}
@@ -1221,7 +1228,7 @@ function NeptunesPrideAgent() {
 		let code = (data && data.split(":")[1]) || otherUserCode;
 		otherUserCode = code;
 		if (otherUserCode) {
-			save_friend(otherUserCode)
+			//save_friend(otherUserCode)
 			let params = { game_number: game, api_version: "0.1", code: otherUserCode };
 			let eggers = jQuery.ajax({ type: 'POST', url: "https://np.ironhelmet.com/api", async: false, data: params, dataType: "json" })
 			let universe = NeptunesPride.universe;
