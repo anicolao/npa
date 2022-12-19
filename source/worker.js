49c50b0e6f63e31fcc921aaf19ced8bef48015ba
Alex Nicolaou <alex@stockgamblers.com>
2022-12-18 23:41:45 -0500

    Use rome for formatting.

--- source/worker.js
+++ source/worker.js
@@ -1,33 +1,37 @@
 /* global chrome, browser */
 
 const add_intel_plugin = () => {
-	var s = document.createElement('script');
-	s.src = chrome.runtime.getURL('intel.js');
-	s.id = "intel";
-	s.title = `Stoned Ape Tools v${chrome.runtime.getManifest().version}`;
-	s.onload = function () {
-		this.remove();
-	};
-	(document.head || document.documentElement).appendChild(s);
-	console.log(`${s.title} background page.`);
-}
-var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-if (isFirefox){
-	console.log("working on Firefox");
-	browser.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
-		if (tab.active) {
-			browser.scripting.executeScript({
-				target: { tabId: tabId },
-				func: add_intel_plugin
-			}).catch(console.log)
-		}
-	})
+  var s = document.createElement("script");
+  s.src = chrome.runtime.getURL("intel.js");
+  s.id = "intel";
+  s.title = `Stoned Ape Tools v${chrome.runtime.getManifest().version}`;
+  s.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
+  console.log(`${s.title} background page.`);
+};
+var isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+if (isFirefox) {
+  console.log("working on Firefox");
+  browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.active) {
+      browser.scripting
+        .executeScript({
+          target: { tabId: tabId },
+          func: add_intel_plugin,
+        })
+        .catch(console.log);
+    }
+  });
 }
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (changeInfo.status === 'complete' && tab.active) {
-		chrome.scripting.executeScript({
-			target: { tabId: tabId },
-			func: add_intel_plugin
-		}).catch(console.log)
-	}
+  if (changeInfo.status === "complete" && tab.active) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        func: add_intel_plugin,
+      })
+      .catch(console.log);
+  }
 });
