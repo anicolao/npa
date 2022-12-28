02b941852f107cfae93363e0044529c7d65f3832
Alex Nicolaou <alex@stockgamblers.com>
2022-12-27 22:55:10 -0500

    Missed the .spec.ts file from the last change.

--- tests/hotkey.spec.ts
+++ tests/hotkey.spec.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import { describe, it } from 'vitest';
 
-import { setClip, lastClip } from '../source/hotkey';
+import { setClip, getClip } from '../source/hotkey';
 
 describe('hotkey tests', () => {
 	it('clip sets lastClip', () => {
-		expect(lastClip).to.equal('Error');
+		expect(getClip()).to.equal('Error');
 		const hello = "Hello, world";
 		setClip(hello);
-		expect(lastClip).to.equal(hello);
+		expect(getClip()).to.equal(hello);
 	});
 });
