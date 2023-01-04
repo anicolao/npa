5853cf8c0485a6c3ed9d1ade274adf30eaa2cdf5
Alex Nicolaou <alex@stockgamblers.com>
2023-01-03 23:26:24 -0500

    Fix ibb URLs to match with embedded slashes

--- src/imageutils.ts
+++ src/imageutils.ts
@@ -1,5 +1,5 @@
 export function safe_image_url(u: string) {
-  return /^https:&#x2F;&#x2F;(i\.ibb\.co|i\.imgur\.com)&#x2F;[-#;\.\w]{3,200}$/.test(
+  return /^https:&#x2F;&#x2F;(i\.ibb\.co|i\.imgur\.com)&#x2F;[-#;\.\w&]{3,200}$/.test(
     u,
   );
 }
