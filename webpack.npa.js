adadeecfaa15febf2a096297561d4e1a650def96
Alex Nicolaou <alex@stockgamblers.com>
2023-02-09 01:42:23 -0500

    Remove sourcemap to fix size problem in prod.

--- webpack.npa.js
+++ webpack.npa.js
@@ -3,7 +3,6 @@ import { common } from './webpack.baseconfig.js';
 
 export default merge(common, {
   mode: 'production',
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
