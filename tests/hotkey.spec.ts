1a6c61721c70c71d9adef5bbc8a37c49f85d21b7
Alex Nicolaou <alex@stockgamblers.com>
2023-02-25 23:22:04 -0500

    Make format format test code, too.

--- tests/hotkey.spec.ts
+++ tests/hotkey.spec.ts
@@ -1,13 +1,13 @@
-import { expect } from 'chai';
-import { describe, it } from 'vitest';
+import { expect } from "chai";
+import { describe, it } from "vitest";
 
-import { setClip, getClip } from '../src/hotkey';
+import { setClip, getClip } from "../src/hotkey";
 
-describe('hotkey tests', () => {
-	it('clip sets lastClip', () => {
-		expect(getClip()).to.equal('Error');
-		const hello = "Hello, world";
-		setClip(hello);
-		expect(getClip()).to.equal(hello);
-	});
+describe("hotkey tests", () => {
+  it("clip sets lastClip", () => {
+    expect(getClip()).to.equal("Error");
+    const hello = "Hello, world";
+    setClip(hello);
+    expect(getClip()).to.equal(hello);
+  });
 });
