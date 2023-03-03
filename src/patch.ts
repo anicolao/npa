c7aed6d650d31b204eb7a375d5fd68e568b74b23
Alex Nicolaou <alex@stockgamblers.com>
2023-03-03 11:56:32 -0500

    Remove unused original diff/patch implementation.
    Having the old version there kept causing me to write incorrect
    tests that mixed the two approaches. Remove the obsolete code.

--- src/patch.ts
+++ src/patch.ts
@@ -1,37 +1,5 @@
 export type Value = boolean | number | string | undefined;
-export type Patch = [string, Patch | Value][] | Value | object;
-const EMPTY = {};
-export function diff<T extends Value | object>(a: T, b: T): Patch {
-  if (a === b) {
-    return a;
-  }
-  if (
-    typeof b === "boolean" ||
-    typeof b === "number" ||
-    typeof b === "string" ||
-    typeof b === "undefined"
-  ) {
-    return b;
-  }
-  let oldA: { [k: string]: Value } = a as { [k: string]: Value };
-  let newA: { [k: string]: Value } = b as { [k: string]: Value };
-  if (oldA === undefined) {
-    return b;
-  }
-  if (typeof b === "object" && typeof a !== "object") {
-    throw `type mismatch, ${typeof a} vs ${typeof b}`;
-  }
-  let ret = Object.entries(oldA)
-    .map((e): [string, Patch | Value] => {
-      return [e[0], diff(oldA[e[0]], newA[e[0]])];
-    })
-    .filter((e) => e[1] !== oldA[e[0]]);
-  ret = ret.concat(Object.entries(b).filter((e) => oldA[e[0]] === undefined));
-  if (ret.length === 0) {
-    return a;
-  }
-  return ret;
-}
+type Patch2 = { [k: string]: any } | Value;
 
 export function clone(o: any) {
   if (typeof o === "object" && o !== null) {
@@ -44,7 +12,6 @@ export function clone(o: any) {
   return o;
 }
 
-type Patch2 = { [k: string]: any } | Value;
 export function diff2<T extends Value | object>(a: T, b: T): Patch2 {
   if (a === b) {
     return null;
@@ -100,35 +67,4 @@ export function patch2(a: Patch2, p: Patch2): Patch2 {
     return newA.filter((x: any) => x !== undefined);
   }
   return newA;
-}
-
-export function patch<T extends Value | object>(a: T, p: Patch) {
-  if (
-    typeof p === "boolean" ||
-    typeof p === "number" ||
-    typeof p === "string" ||
-    typeof p === "undefined" ||
-    a === p
-  ) {
-    return p;
-  }
-  let ret: { [k: string]: any } = a as any;
-  if ((p as any).forEach) {
-    (p as any).forEach((pair: [string, Patch]) => {
-      if (ret[pair[0]] === undefined) {
-        ret[pair[0]] = {};
-      }
-      ret[pair[0]] = patch(ret[pair[0]], pair[1]);
-    });
-  } else {
-    ret = p;
-  }
-  if (ret.filter) {
-    return ret.filter((x: any) => x !== undefined);
-  }
-  const deleteKeys = Object.entries(ret)
-    .filter((e) => e[1] === undefined)
-    .map((e) => e[0]);
-  deleteKeys.forEach((k) => delete ret[k]);
-  return ret;
 }
