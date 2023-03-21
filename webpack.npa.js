600fd67c68d99590f4eaaa1430de4050a9e51136
Alex Nicolaou <alex@stockgamblers.com>
2023-03-21 15:23:54 -0400

    Record source map as part of the build process.
    To make it easy to map production stack traces back to source,
    record the source map each time a production build is done and
    put it next to the zip files as intel.{version}.map.
    
    Then you can
    
    npm install -g source-map-cli
    
    and get convenient output like:
    
    $ source-map resolve intel.v2.2.29.map 2 257317
    Maps to webpack://neptunes-pride-agent/src/scans.ts:73:24 (getScanData)
    
            return h.peek().getScanData();
                            ^

--- webpack.npa.js
+++ webpack.npa.js
@@ -3,6 +3,7 @@ import { common } from './webpack.baseconfig.js';
 
 export default merge(common, {
   mode: 'production',
+  devtool: 'hidden-source-map',
   devServer: {
     static: './dist',
   },
