287f4e3752e4525744599d6bedb40f759aa605cd
Alex Nicolaou <alex@stockgamblers.com>
2023-06-10 06:00:53 -0400

    Formatting fix, presubmit was defeated accidentally.

--- src/intel.ts
+++ src/intel.ts
@@ -4610,7 +4610,9 @@ function NeptunesPrideAgent() {
         for (let i = 1; i < 10; ++i) {
           const betterTick = Math.ceil(remaining / researchRate(science + i));
           if (betterTick < tickIncr) {
-            upgrade = `${i}<sub style="font-size: 50%">${tickIncr - betterTick}</sub>`;
+            upgrade = `${i}<sub style="font-size: 50%">${
+              tickIncr - betterTick
+            }</sub>`;
             break;
           }
         }
