adad94d8b1f282e2d86485958bb12800f6efce03
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 14:33:11 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -40,7 +40,7 @@ export function futureTime(
   for (const fk in fleets) {
     if (fleets[fk].o.length > 0) {
         const newFleet = {...fleets[fk]};
-        const [delay, staruid, action, argument] = 
+        const [delay, staruid, action, argument] = fleets[fk]
         const destUid = newFleet.o[0][1];
         const destination = stars[destUid];
         const [destX, destY] = [parseFloat(destination.x), parseFloat(destination.y)];
