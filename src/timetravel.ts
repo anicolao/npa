5672840e92ef44337f63b5eb5c2fcca0137edda6
Alex Nicolaou <alex@stockgamblers.com>
2023-08-12 22:21:24 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -20,7 +20,7 @@ export function futureTime(
   for (const sk in stars) {
     const star = stars[sk];
     if (star.v) {
-        if (star.)
+        const scannedStar = 
     }
   }
   newState.stars = stars;
