e123cbd919ac0f28af1a73d57dc28d3a19b9636c
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 15:41:53 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -55,7 +55,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
   const players = galaxy.players;
   let fleets = galaxy.fleets;
   let stars = galaxy.stars;
-  let flights:  = [];
+  let flights: [] = [];
   fleetOutcomes = {};
   for (const f in fleets) {
     let fleet = fleets[f];
