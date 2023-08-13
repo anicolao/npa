c1e70493da59ee5964ff567a8c7588291f1151e1
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 16:28:09 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -447,6 +447,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
             fleetOutcomes[fleet.uid] = {
               eta: `[[Tick #${absoluteTick(galaxy, fleet.etaFirst)}]]`,
               outcome: outcomeString,
+              strength: fleet.st
             };
           }
         }
