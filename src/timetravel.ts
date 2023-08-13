544e139937cd35a0011f1bb12e55111570394279
Alex Nicolaou <alex@stockgamblers.com>
2023-08-12 22:31:01 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -28,7 +28,7 @@ export function futureTime(
             const production = (industry*(manufacturing+5))*tickOffset/ticksPerDay;
             const newStar = {...star};
             newStar.st += production;
-            newStar.totalDefenses += 10;
+            newStar.totalDefenses += production;
             stars[sk] = newStar;
         }
     }
