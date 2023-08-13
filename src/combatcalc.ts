310692f0b7d375de8f3e229ab2c6c947cc1a970e
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 15:22:56 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -1,6 +1,7 @@
 import { ScanningData } from "./galaxy";
 import { Stanzas } from "./reports";
 
+export 
 export let knownAlliances: number[][] | undefined = undefined;
 export let combatHandicap = 0;
 
