e6bd3b12675e3cc6a6d6a307211abca26d862746
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 16:20:48 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -15,33 +15,33 @@ export function futureTime(
   for (let i = 0; i < tickOffset; ++i) {
     const staroutcomes: { [k: string]: StarState } = {};
     computeCombatOutcomes(newState, staroutcomes);
-  newState.tick += 1;
-  if (tickOffset <= 0) {
-    console.error("Future time machine going backwards NIY")
-    logCount("error_back_to_the_future");
-    return newState;
-  }
-  const players = newState.players;
-  const stars = {...newState.stars};
-  for (const sk in stars) {
-    const star = stars[sk];
-    if (star.v === "1") {
-        if (star.i > 0) {
-            const ticksPerDay = newState.production_rate;
-            const industry = star.i;
-            const manufacturing = players[star.puid].tech.manufacturing.level;
-            const production = (industry*(manufacturing+5))*tickOffset/ticksPerDay;
-            const newStar = {...star};
-            newStar.st += production + newStar.c;
-            newStar.c = newStar.st - Math.floor(newStar.st);
-            newStar.st = Math.floor(newStar.st);
-            newStar.totalDefenses += newStar.st - star.st;
-            stars[sk] = newStar;
-        }
+    newState.tick += 1;
+    if (tickOffset <= 0) {
+      console.error("Future time machine going backwards NIY")
+      logCount("error_back_to_the_future");
+      return newState;
     }
-  }
-  newState.stars = stars;
-  const fleets = {...newState.fleets};
+    const players = newState.players;
+    const stars = {...newState.stars};
+    for (const sk in stars) {
+      const star = stars[sk];
+      if (star.v === "1") {
+          if (star.i > 0) {
+              const ticksPerDay = newState.production_rate;
+              const industry = star.i;
+              const manufacturing = players[star.puid].tech.manufacturing.level;
+              const production = (industry*(manufacturing+5))*tickOffset/ticksPerDay;
+              const newStar = {...star};
+              newStar.st += production + newStar.c;
+              newStar.c = newStar.st - Math.floor(newStar.st);
+              newStar.st = Math.floor(newStar.st);
+              newStar.totalDefenses += newStar.st - star.st;
+              stars[sk] = newStar;
+          }
+      }
+    }
+    newState.stars = stars;
+    const fleets = {...newState.fleets};
     computeCombatOutcomes(newState, staroutcomes);
     for (const fk in fleets) {
       if (fleets[fk].o.length > 0) {
