31b698f8513e9236cdae188e9b8aa5bdeac1e811
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 14:48:28 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -65,7 +65,7 @@ export function futureTime(
               newFleet.warpSpeed = (nextDestination.ga === destination.ga) ? nextDestination.ga : 0;
               const speed = newState.fleet_speed * (newFleet.warpSpeed ? 3 : 1);
               const ticks = Math.ceil(dist(destination, nextDestination) / speed);
-
+              
             } else {
               newFleet.etaFirst = 0;
             }
