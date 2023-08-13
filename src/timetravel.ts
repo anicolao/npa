56b5abbeb4df411959033745678e53352439ea1f
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 14:40:40 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -60,6 +60,7 @@ export function futureTime(
             newFleet.o = newFleet.o.slice(1);
             if (newFleet.o.length > 0) {
               const nextDestUid = fleets[fk].o[0][1];
+              
               newFleet.warpSpeed = 
             } else {
               newFleet.etaFirst = 0;
