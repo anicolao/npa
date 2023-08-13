5e62f04492612ccaacbd52c7333952efe30b0c2d
Alex Nicolaou <alex@stockgamblers.com>
2023-08-13 15:18:39 -0400

    WIP on future time machine

--- src/intel.ts
+++ src/intel.ts
@@ -52,7 +52,7 @@ import {
 import { getCodeFromApiText, ScanKeyIterator, TickIterator } from "./scans";
 import { isSafari } from "./useragent";
 import { futureTime } from "./timetravel";
-import { knownAlliances, StarState } from "./combatcalc.js";
+import { combatOutcomes, knownAlliances, StarState } from "./combatcalc.js";
 
 interface CruxLib {
   IconButton: any;
