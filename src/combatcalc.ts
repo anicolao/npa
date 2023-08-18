8b5fa0c39ad3c838d9b114d5516222f985a2830c
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 20:37:32 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -418,7 +418,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
         starstate[starId].ships = 0;
         offense = newAggregate;
         for (let k in starstate[starId].fleetStrength) {
-          const puid = parseInt(ka[0]);
+          const puid = 
           if (alliedFleet(galaxy.players, biggestPlayerId, puid)) {
             offense -= fleetStrength[k];
             starstate[starId].ships += fleetStrength[k];
