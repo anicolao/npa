82ac07dd6ba8a66e36ba4221641ac79c3a1559f8
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 23:45:17 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -90,6 +90,8 @@ export function futureTime(
               break;
             case FleetOrder.CollectAll:
               newFleet.st += stars[destUid].st;
+              stars[destUid].st = 0;
+              break;
               
           }
           
