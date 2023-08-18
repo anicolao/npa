457bb033088e9ad477a44ffcc46f733fee395758
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 22:32:13 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -312,7 +312,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
       }
     }
     let attackersAggregate = offense;
-    let attackersAggregate = d;
+    let attackersAggregate = defense;
     while (offense > 0) {
       let dwt = starstate[starId].weapons;
       let defense = starstate[starId].ships;
