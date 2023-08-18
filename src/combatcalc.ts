9c20f7ff9eacbb5cf0bfe33e235a6f8926019eb9
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 22:47:15 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -471,7 +471,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
         let roundOffDebt = 0;
         for (let i = 0; i < pairs.length; ++i) {
           let k = pairs[i][0];
-          let fleet = k != "star" ? fleets[k];
+          let fleet = k != "star" ? fleets[k] ;
           if (!alliedFleet(galaxy.players, fleet.puid, starstate[starId].puid)) {
             starstate[starId].fleetStrength[fleet.uid] = 0;
           } else {
