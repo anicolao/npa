0ddd2dedc99ac9d3da4b5a6f0223033abd41c3ca
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 20:59:08 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -52,7 +52,7 @@ export const combatOutcomes = (staroutcomes?: { [k: string]: StarState }) => {
   return computeCombatOutcomes(galaxy, staroutcomes);
 }
 
-export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k: string]: StarState }, ) => {
+export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k: string]: StarState }, maxTick: ) => {
   console.log("NEW ComputeCombatOutcomes called")
   const players = galaxy.players;
   let fleets = galaxy.fleets;
