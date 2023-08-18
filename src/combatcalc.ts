51a5cc46e2a20eaea5cfd37abbe3ec8ee3bdc473
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 23:27:03 -0400

    WIP on future time machine

--- src/combatcalc.ts
+++ src/combatcalc.ts
@@ -144,7 +144,7 @@ export const computeCombatOutcomes = (galaxy: ScanningData, staroutcomes?: { [k:
             } else {
               const leaving =
               departures[delay - 1].leaving + fleet.st;
-              starstate[starId].departures[delay - 1] = { leaving, origShips };
+              departures[delay - 1] = { leaving, origShips };
             }
             const leaving =
               starstate[starId].departures[delay - 1].leaving + fleet.st;
