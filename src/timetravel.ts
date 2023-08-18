978cf4d9915356c7b6dd82437692d8c7980228c4
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 23:51:26 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -96,6 +96,7 @@ export function futureTime(
               transferred = -newStar.st;
               break;
             case FleetOrder.Collect:
+              transferred = -newStar.st;
               const amount = action === FleetOrder.CollectAll ? newStar.st : Math.min(newStar.st, argument);
               break;
             default:
