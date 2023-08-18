b1cb77df0cca5e75ebfb0b77e6aa0fb31cc66356
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 21:04:33 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -72,7 +72,7 @@ export function futureTime(
 
           // Update fleet as a result of battle
           let starstate = staroutcomes[destUid];
-          if (starstate) {
+          if (starstate && starstate.fleetStrength[newFleet.uid]) {
             newFleet.st = starstate.fleetStrength[newFleet.uid];
           }
           newFleet.orbiting = destUid;
