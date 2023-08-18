bf4d0cc0afd6c58ebf56bdd813ea94f0dab4e38b
Alex Nicolaou <alex@stockgamblers.com>
2023-08-17 23:56:11 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -111,7 +111,7 @@ export function futureTime(
               transferred = Math.max(0, newFleet.st - argument);
               break;
             case FleetOrder.Garrison:
-              transferred = 
+              transferred = -newStar.st
               break;
             default:
               break;
