5b1163221607edbd26e4bc3f3bae01a0a5ef3f46
Alex Nicolaou <alex@stockgamblers.com>
2023-09-11 13:12:43 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -67,7 +67,6 @@ export function futureTime(
               newFleet.orbiting.ga === destination.ga ? destination.ga : 0;
             newFleet.w = newFleet.warpSpeed;
             if (newFleet.uid === NeptunesPride.universe.selectedFleet.uid) {
-              console.log(`Fleet ${newFleet.n} @ warp ${newFleet.w}`)
               console.log(`Fleet ${newFleet.n} @ warp ${newFleet.w} ETA ${newFleet.etaFirst} to ${nextDestUid}`)
             }
         }
