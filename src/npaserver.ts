92d47addd92b7ffa66061ab2fb8873c7d97afc52
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 19:54:30 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -27,7 +27,7 @@ export const scanInfo: { [k: string]: ApiInfo } = {};
 
 eport interface CachedScan {
   apis?: string;
-cached?>>:
+  cached?: any;
 forward
 next
 notifications : "117781118035028495985"
