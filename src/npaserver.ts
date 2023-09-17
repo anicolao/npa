6f717fb9043da1dfe0c2e06acebc8653749528fe
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 19:32:57 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -258,7 +258,7 @@ export async function getServerScans(apikey: string) {
   const unsubDiffs = onSnapshot(
     query(
       collection(firestore, diffskey),
-      where("latest", ">", timestamp * 0.5),
+      where("latest", ">", timestamp * 0),
       orderBy("latest"),
     ),
     (querySnapshot) => {
