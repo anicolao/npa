1bf7f2ead52f32f2fa69a61e088520d3247f7323
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 20:13:05 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -278,7 +278,9 @@ export async function getServerScans(apikey: string) {
         let doc = change.doc;
         let patches = doc.data() as any;
         const timestamps: number[] = Object.keys(patches).filter(x => +x > 0).map(x => +x).sort();
-        if (timestamps.length)
+        if (timestamps.length > 0) {
+          
+        }
         timestamps.forEach(timestamp => {
           const forward = JSON.parse(patches[timestamp].stringValue);
           if (diffCache[apikey] === undefined) {
