6685899b8567c2ca5e0a8af82ab977d1bad437f9
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 23:10:32 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -97,6 +97,7 @@ export async function restoreFromDB(gameId: number, apikey: string, newDB: boole
       if (newDB) {
       diffCache[apikey] = scanCache[apikey] = await restore(gameId, apikey);
       }
+      diffCache[apikey] = scanCache[apikey] = await restore(gameId, apikey);
       console.log(`Restored scan cache from db: ${scanCache[apikey].length}`);
     } catch (err) {
       console.error(err);
