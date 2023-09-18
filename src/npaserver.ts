9985b0d2b853a7d4edd78b7f05a96394bdbdcedd
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 20:47:23 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -298,6 +298,7 @@ export async function getServerScans(apikey: string) {
           diffCache[apikey].push({
             cached, back
           });
+          if (last > 0) 
           diffCache[apikey][last].cached = undefined;
         });
         console.log("Diff update received: ", change, diffCache, scanCache);
