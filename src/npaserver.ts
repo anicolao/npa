1df5a3e68d9d4771bda009dd727bf0dabbd30817
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 00:39:56 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -37,6 +37,7 @@ export interface CachedScan {
 }
 const scanCache: { [k: string]: any[] } = {};
 const diffCache: { [k: string]: any[] } = {};
+        
         function validateEntry(entry: any, i: number, skipCached?: boolean): void {
           const scanCacheEntry = scanCache[apikey][i];
           if (entry.timestamp !== scanCacheEntry.timestamp) {
