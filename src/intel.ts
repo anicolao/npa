554990613f76978682c54be8cf4c293e00a65e6a
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 09:28:33 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1231,7 +1231,6 @@ function NeptunesPrideAgent() {
           }
         }
       }
-      return starOwnerId === fleetOwnerId;
     }
     const players = NeptunesPride.universe.galaxy.players;
     const fOwner = players[fleetOwnerId];
