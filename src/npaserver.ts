2f9045f658680512d81a34b3bcf0a612c8588af3
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 22:24:13 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -478,14 +478,14 @@ export function getScan(apikey: string, index: number): ScanningData & { eof?: b
   const oldRet = parseScan(scans[index]);
   try {
     if (diffCache[apikey]) {
-    const newRet = walkToScan(apikey, index);
-    const nullDiff = diff(oldRet, newRet);
-    if (nullDiff !== null) {
-      console.error(`getScan return values won't match `, oldRet, newRet);
-    } else {
-      console.log(`Success on ${apikey} @ ${index}!`)
+      const newRet = walkToScan(apikey, index);
+      const nullDiff = diff(oldRet, newRet);
+      if (nullDiff !== null) {
+        console.error(`getScan return values won't match `, oldRet, newRet);
+      } else {
+        console.log(`Success on ${apikey} @ ${index}!`)
+      }
     }
-  }
   } catch (err) {
     console.error(err);
     logCount(err);
