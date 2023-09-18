24cf7b1681d892e200dac19849e4056770f5338f
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 08:38:30 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1364,6 +1364,8 @@ function NeptunesPrideAgent() {
         starstate[starId].puid = owner;
       }
       for (const i in arrival) {
+      let ka = k.split(",");
+      let tick = parseInt(ka[0]);
         let fleet = arrival[i];
         if (alliedFleet(fleet.puid, starstate[starId].puid)) {
           const weapons = Math.max(
