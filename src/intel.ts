b27cca78bdd422f86d4b10b3c8ceca090288a207
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 21:37:59 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -4232,7 +4232,7 @@ function NeptunesPrideAgent() {
           return null;
         }
         //console.log({ timeTravelTickIndex, len: scans.length, targetTick });
-        scan = getScan(scans, timeTravelTickIndex);
+        scan = getScan(api, timeTravelTickIndex);
         scan = adjustNow(scan);
       }
     } else if (scan.tick > targetTick) {
