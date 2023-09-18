550a11e42e2e1baad6210dfc56e86b618d9e113a
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 18:57:24 -0400

    WIP on future time machine

--- src/intel.ts
+++ src/intel.ts
@@ -3785,6 +3785,7 @@ function NeptunesPrideAgent() {
           const newGalaxy = futureTime(NeptunesPride.universe.galaxy, tickOffset);
           resetAliases();
           NeptunesPride.np.onFullUniverse(null, newGalaxy);
+          console.log
         } else if (dir === "back") {
           warpTime(null, `${trueTick}`);
         }
