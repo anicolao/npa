4f8511e5211177a0455130db55bffeb628cd73f4
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 22:17:03 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -40,7 +40,7 @@ const diffCache: { [k: string]: any[] } = {};
 
 export function countScans(apikey: string) {
   if (scanCache[apikey] && diffCache[apikey])
-    return scanCache[apikey].length;
+    return Math.min(scanCache[apikey].length;
   return 0;
 }
 
