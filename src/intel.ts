d21bdd8ab9f56d6a58c76beb850b030a5b9e2edb
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 08:53:07 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1201,6 +1201,7 @@ function NeptunesPrideAgent() {
     let annals = [];
     if (knownAlliances === undefined && NeptunesPride.gameConfig.alliances) {
       faReport();
+      annals = annalsO
       console.log({knownAlliances, relativeTick, tick: tickNumber(relativeTick), fleetOwnerId, starOwnerId});
     }
     const players = NeptunesPride.universe.galaxy.players;
