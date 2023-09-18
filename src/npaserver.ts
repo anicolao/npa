5f9356307ba74ab1983056ad42c3e8a26709e63a
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 21:53:41 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -438,7 +438,7 @@ function parseScan(scan: any) {
 
 let lastScan: { [k: string]: number } = {};
 function walkToScan(apikey: string, index: number) {
-  
+  const last = lastScan[apikey]?.
 }
 export function getScan(apikey: string, index: number): ScanningData & { eof?: boolean } {
   const scans = scanCache[apikey];
