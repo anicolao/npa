8959f1e7279a518caf766295dc540f15c6ed2b96
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 09:15:28 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1216,7 +1216,7 @@ function NeptunesPrideAgent() {
             console.log(`Stop checking @ ${annal.tick} v ${currentTick}`)
             break;
           }
-          if (annal.tick > tic) {
+          if (annal.tick > tick) {
             console.log(`Skip future tick ${annal.tick}`)
             continue;
           }
