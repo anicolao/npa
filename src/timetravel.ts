8131c60cfd10c05aa9c13d87e44485a655dfd405
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 19:09:29 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -27,8 +27,7 @@ export function resetAliases() {
   for (const pk in universe.galaxy.players) {
     const player = universe.galaxy.players[pk];
     player.alias = player.rawAlias.split(space)[0];
-    player.rawAlias = player.alias;
-    if (player.ai === 1 || player.ready === 1) {
+    player.rawAlias = player.alias; if (player.ai === 1 || player.ready === 1) {
       player.alias += `${space}${modSymbols[player.modTick]} `
     }
   }
