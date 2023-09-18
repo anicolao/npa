62c94f71955d7bad0cc14c5ab9ea5cd10b515bf8
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 00:23:55 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -345,7 +345,7 @@ export async function getServerScans(apikey: string) {
             }
           }
         }
-        diffCache[apikey].forEach(validateEntry)
+        diffCache[apikey].forEach(validateEntry);1
       });
     },
     (error) => {
