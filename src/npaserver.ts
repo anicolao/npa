0f35e474d9a4fb34061ea8da317ba35f780aca52
Alex Nicolaou <alex@stockgamblers.com>
2023-09-17 23:11:38 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -94,7 +94,7 @@ const firestore = initializeFirestore(app, {
 export async function restoreFromDB(gameId: number, apikey: string) {
   if (!scanCache[apikey] || scanCache[apikey].length === 0) {
     try {
-      diffCache[apikey] = await restore(gameId, apikey, "");
+      diffCache[apikey] = await restore(gameId, apikey, "diffCache");
       scanCache[apikey] = await restore(gameId, apikey, false);
       console.log(`Restored scan cache from db: ${scanCache[apikey].length}`);
     } catch (err) {
