fa2a163630f756ce21c2219b826497a6bab4ca6f
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 09:18:52 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1203,7 +1203,7 @@ function NeptunesPrideAgent() {
       faReport();
     }
     if (NeptunesPride.gameConfig.alliances) {
-      return false;
+      return false
       if (relativeTick > 0) {
         annals = annalsOfWar().sort((a, b) => b.tick - a.tick);
         const currentTick = tickNumber(0);
