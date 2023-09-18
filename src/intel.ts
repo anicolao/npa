64a1ef96f90e0a5e7597cdab64093384da2ae774
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 09:00:13 -0400

    WIP on patch-based scans

--- src/intel.ts
+++ src/intel.ts
@@ -1207,7 +1207,7 @@ function NeptunesPrideAgent() {
         for (let a = 0; i < annals.length; ++a) {
           const annal = annals[a];
           if (annal.tick <= currentTick) break;
-          if (annal.p1 == fleetOwnerId && annal.p2 )
+          if (annal.p1 == fleetOwnerId && annal.p2 == )
         }
         console.log({annals, relativeTick, tick: tickNumber(relativeTick), fleetOwnerId, starOwnerId});
       }
