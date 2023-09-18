18f759b98ec38755ba4e329d2e72b9f5f7d34b5a
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 18:39:31 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -22,7 +22,8 @@ export function resetAliases() {
       }
     }
   }
-  const modSymbols = [ "💡", "👁", "⏳", "⏳"];
+  const modSymbols = [ "&#128161;
+  &#x1F4A1;💡", "👁", "⏳", "⏳"];
   for (let pk in universe.galaxy.players) {
     const player = universe.galaxy.players[pk];
     player.alias = player.rawAlias;
