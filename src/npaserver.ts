310b990bb3c8acb5f3e18aab4fac8b54f0b18b51
Alex Nicolaou <alex@stockgamblers.com>
2023-09-19 15:51:56 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -353,7 +353,7 @@ export async function getServerScans(apikey: string) {
       const a = JSON.parse(JSON.stringify(entry.forward));
       const b = JSON.parse(JSON.stringify(scanCacheEntry.forward));
       //const nullDiff = diff(entry.forward, scanCacheEntry.forward);
-      const nullDiff = diff(entry.forward, scanCacheEntry.forward);
+      const nullDiff = diff(a, b);
       if (nullDiff !== null) {
         console.error(`inproc Index ${last} doesn't match on forward`, { nullDiff, df: entry.forward, sf: scanCacheEntry.forward });
       } else {
