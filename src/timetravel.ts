e89d054c4794423a5ca782091550c4251e48e8d0
Alex Nicolaou <alex@stockgamblers.com>
2023-09-18 20:52:50 -0400

    WIP on future time machine

--- src/timetravel.ts
+++ src/timetravel.ts
@@ -22,7 +22,7 @@ export function resetAliases() {
   }
   const space = "\u00A0";
   //const modSymbols = [ "\uD83D\uDCA1", "\uD83D\uDC41", "\u23F3", "\u23F3"];
-  const modSymbols = [ "\uD83D\uDCA1", "\uD83D\uDC41", "\u23F3", "\u23F3"];
+  const modSymbols = [ "\u20e3A1", "\uD83D\uDC41", "\u23F3", "\u23F3"];
   for (const pk in universe.galaxy.players) {
     const player = universe.galaxy.players[pk];
     player.alias = player.rawAlias.split(space)[0];
