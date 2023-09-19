cd1cdbf20b8830315cdaf78e0d48a9da64a2a09b
Alex Nicolaou <alex@stockgamblers.com>
2023-09-19 15:28:14 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -354,6 +354,7 @@ export async function getServerScans(apikey: string) {
         console.error(`inproc Index ${last} doesn't match on forward`, { nullDiff, df: entry.forward, sf: scanCacheEntry.forward });
       } else {
         console.log(`inproc timestamp match for ${last}`)
+      }
     }
           const priorCache = window.structuredClone(diffCache[apikey][last].cached);
           const cached = patch(priorCache, forward);
