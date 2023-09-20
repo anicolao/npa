6fa4098b804bbc61b2d27435a85328fe353ee3d4
Alex Nicolaou <alex@stockgamblers.com>
2023-09-19 23:55:00 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -356,6 +356,7 @@ export async function getServerScans(apikey: string) {
     const scanCacheEntry = scanCache[apikey][last];
     const entry = diffCache[apikey][last];
     if (entry.timestamp !== timestamp) {
+      console.error(`Unepected timestamp ${entry.timestamp} instead of ${timestam}`)
     }
     if (entry.timestamp !== scanCacheEntry.timestamp && last !== 0) {
       console.error(`inproc TS mismatch for ${apikey}:${last}: ${entry.timestamp} vs ${scanCacheEntry.timestamp}`, entry, scanCacheEntry)
