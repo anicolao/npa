d7a904ad86e81f21ae406c2ba16762452ccf17b6
Alex Nicolaou <alex@stockgamblers.com>
2023-09-19 23:58:27 -0400

    WIP on patch-based scans

--- src/npaserver.ts
+++ src/npaserver.ts
@@ -358,7 +358,7 @@ export async function getServerScans(apikey: string) {
     if (scanCache[apikey].length > last) {
       const nextEntry = scanCache[apikey][last+1];
       if (timestamp !== nextEntry.timestamp) {
-        console.error(`Unexpected forward timestamp ${timestamp} vs ${}`)
+        console.error(`Unexpected forward timestamp ${timestamp} vs ${nextEntry}`)
       }
     }
     if (entry.timestamp !== scanCacheEntry.timestamp && last !== 0) {
