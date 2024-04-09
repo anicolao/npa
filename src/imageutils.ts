c6b723507b150659ddec15a37ce73b15dd7dff74
Alex Nicolaou <alex@stockgamblers.com>
2024-04-09 11:06:27 -0400

    Fix regexps for image inlining

--- src/imageutils.ts
+++ src/imageutils.ts
@@ -1,11 +1,11 @@
 export function safe_image_url(u: string) {
-  return /^https:[&#x2F;/][&#x2F;/](i\.ibb\.co|i\.imgur\.com)[&#x2F;/][-#;\.\w&/]{3,200}$/.test(
+  return /^https:(&#x2F;|\/)(&#x2F;|\/)(i\.ibb\.co|i\.imgur\.com)(&#x2F;|\/)[-#;\.\w&/]{3,200}$/.test(
     u,
   );
 }
 
 export function youtube(u: string) {
-  return /^https:[&#x2F;/][&#x2F;/]www\.youtube\.com[&#x2F;/]watch\?v=[-\w]{6,50}$/.test(
+  return /^https:(&#x2F;|\/)(&#x2F;|\/)www\.youtube\.com(&#x2F;|\/)watch\?v=[-\w]{6,50}$/.test(
     u,
   );
 }
