4215fe3404417f9103b9677bc88663ae4f3baaa6
Alex Nicolaou <alex@stockgamblers.com>
2024-05-03 23:45:17 -0400

    Add support for arbitrary player count to game creation.
    Using the +/- hotkeys you can set the number of players to any
    desired number. No guarantees the server will like it, but it seems
    to work for numbers < 64.

--- src/intel.ts
+++ src/intel.ts
@@ -181,7 +181,11 @@ function NeptunesPrideAgent() {
     if (NeptunesPride?.np?.on) {
       NeptunesPride.np.on(trigger, fn);
     } else {
-      console.log(`NP not initialied yet, defer trigger for ${trigger}`);
+      if (NeptunesPride.MetaGame) {
+        console.log(`In metagame screen, stop; trigger ${trigger}`);
+        return;
+      }
+      console.log(`NP not initialized yet, defer trigger for ${trigger}`);
       window.setTimeout(() => onTrigger(trigger, fn), 100);
     }
   }
@@ -1291,7 +1295,11 @@ function NeptunesPrideAgent() {
       });
       //console.log("rebuild color map after ", JSON.stringify(colorMap));
     }
-    if (galaxy.players[1].shape !== undefined && shapeMap && NeptunesPride.gameVersion === "proteus") {
+    if (
+      galaxy.players[1].shape !== undefined &&
+      shapeMap &&
+      NeptunesPride.gameVersion === "proteus"
+    ) {
       shapeMap = shapeMap.map((_, i) => {
         const uid = i + (isNP4() ? 1 : 0);
         if (galaxy.players[uid] !== undefined) {
@@ -3727,87 +3735,95 @@ function NeptunesPrideAgent() {
     const superEmpireDirectory = npui.EmpireDirectory;
     npui.EmpireDirectory = function () {
       console.log("Empires page hooked");
-      let starDir = npui.Screen("galaxy")
-        .size(480)
-  
-      npui.DirectoryTabs("emp")
-        .roost(starDir)
-  
-      let header = Crux.Widget("rel col_accent")
-        .size(480, 48)
-        .roost(starDir)
-  
+      let starDir = npui.Screen("galaxy").size(480);
+
+      npui.DirectoryTabs("emp").roost(starDir);
+
+      let header = Crux.Widget("rel col_accent").size(480, 48).roost(starDir);
+
       let pageHTML = `
         <a onPointerUp="Crux.crux.trigger('emp_dir_page', 'inf:raw')">Infrastructure</a> |
         <a onPointerUp="Crux.crux.trigger('emp_dir_page', 'inf:prod')">Production</a> |
         <a onPointerUp="Crux.crux.trigger('emp_dir_page', 'inf:tech')">Technology</a> |
         <a onPointerUp="Crux.crux.trigger('emp_dir_page', 'col')">Colors</a>
-      `
-      Crux.Text("", "pad12 col_accent")
-         .rawHTML(pageHTML)
-        .roost(header)
-  
+      `;
+      Crux.Text("", "pad12 col_accent").rawHTML(pageHTML).roost(header);
+
       const universe = NeptunesPride.universe;
       if (universe.empireDirectory.page === "col") {
         Crux.IconButton("icon-loop", "player_color_shape_reset_all")
           .grid(27, 0, 3, 3)
-          .roost(header)
+          .roost(header);
         Crux.IconButton("icon-light-up", "player_color_shape_zero_all")
           .grid(24.5, 0, 3, 3)
-          .roost(header)
+          .roost(header);
       }
-  
-  
-      let sortedEmpires = Object.values(universe.galaxy.players).map(decorate => {
-        const ret = {...decorate};
-        const techs = Object.keys(decorate.tech);
-        for (const k of techs) {
-          ret[`tech_${k}`] = decorate.tech[k].level;
+
+      let sortedEmpires = Object.values(universe.galaxy.players).map(
+        (decorate) => {
+          const ret = { ...decorate };
+          const techs = Object.keys(decorate.tech);
+          for (const k of techs) {
+            ret[`tech_${k}`] = decorate.tech[k].level;
+          }
+          return ret;
         }
-        return ret;
-      })
-  
+      );
+
       if (universe.empireDirectory.sortBy === "name") {
         sortedEmpires.sort(function (a, b) {
-          let result = -1
+          let result = -1;
           if (a.n < b.n) {
-            result = 1
+            result = 1;
           }
-          result *= universe.empireDirectory.invert
-          return result
-        })
+          result *= universe.empireDirectory.invert;
+          return result;
+        });
       } else {
         sortedEmpires.sort(function (a, b) {
-          let result = b[universe.empireDirectory.sortBy] - a[universe.empireDirectory.sortBy]
+          let result =
+            b[universe.empireDirectory.sortBy] -
+            a[universe.empireDirectory.sortBy];
           if (result === 0) {
-            result = 1
+            result = 1;
             if (a.n < b.n) {
-              result = -1
+              result = -1;
             }
           }
-          result *= universe.empireDirectory.invert
-          return result
-        })
+          result *= universe.empireDirectory.invert;
+          return result;
+        });
       }
-  
-      let html = ""
+
+      let html = "";
       if (universe.empireDirectory.page.startsWith("inf")) {
         const raw = [
           { title: '<span class="icon-star-1"></span>', field: "totalStars" },
-          { title: '<span class="icon-rocket"></span>', field: "totalStrength" },
+          {
+            title: '<span class="icon-rocket"></span>',
+            field: "totalStrength",
+          },
           { title: "E", field: "totalEconomy" },
           { title: "I", field: "totalIndustry" },
           { title: "S", field: "totalScience" },
         ];
         const prod = [
           { title: '<span class="icon-star-1"></span>', field: "totalStars" },
-          { title: '<span class="icon-rocket"></span>', field: "totalStrength" },
-          { title: '<span class="icon-rocket"></span>/h', field: "shipsPerTick" },
-          { title: '$', field: "cashPerDay" },
+          {
+            title: '<span class="icon-rocket"></span>',
+            field: "totalStrength",
+          },
+          {
+            title: '<span class="icon-rocket"></span>/h',
+            field: "shipsPerTick",
+          },
+          { title: "$", field: "cashPerDay" },
           { title: "S", field: "totalScience" },
         ];
         const techs = Object.keys(NeptunesPride.universe.player.tech);
-        const tech = techs.map((x) => { return { title: translateTechEmoji(x), field: `tech_${x}` } });
+        const tech = techs.map((x) => {
+          return { title: translateTechEmoji(x), field: `tech_${x}` };
+        });
         const selector = universe.empireDirectory.page.split(":")[1];
         let fields = { raw, prod, tech }[selector];
         if (fields === undefined) fields = raw;
@@ -3815,24 +3831,32 @@ function NeptunesPrideAgent() {
         <tr><td><a onPointerUp="Crux.crux.trigger('emp_dir_sort', 'uid')">P</a></td>
         <td class='star_directory_name'><a onPointerUp="Crux.crux.trigger('emp_dir_sort', 'name')">Name</a></td>
         <td></td>
-        ${fields.map(column => `<td><a onPointerUp="Crux.crux.trigger('emp_dir_sort', '${column.field}')">${column.title}</a></td>`).join('')}
-        </tr>`
-  
+        ${fields
+          .map(
+            (column) =>
+              `<td><a onPointerUp="Crux.crux.trigger('emp_dir_sort', '${column.field}')">${column.title}</a></td>`
+          )
+          .join("")}
+        </tr>`;
+
         for (let empire of sortedEmpires) {
           html += `
             <tr>
             <td> ${empire.hyperlinkedBox} </td>
             <td> ${empire.hyperlinkedAlias} </td>
-            <td> <a onPointerUp="Crux.crux.trigger('show_player_home_uid', ${empire.uid})" class="ic-eye">&#59146;</a></td>
-            ${fields.map(column => `<td> ${empire[column.field]} </td>`).join('')}
+            <td> <a onPointerUp="Crux.crux.trigger('show_player_home_uid', ${
+              empire.uid
+            })" class="ic-eye">&#59146;</a></td>
+            ${fields
+              .map((column) => `<td> ${empire[column.field]} </td>`)
+              .join("")}
             </tr>
-          `
+          `;
         }
-        html += "</table>"
-  
+        html += "</table>";
       }
-  
-          if (universe.empireDirectory.page === "col") {
+
+      if (universe.empireDirectory.page === "col") {
         html = `<table class='star_directory'>
         <tr><td><a onPointerUp="Crux.crux.trigger('emp_dir_sort', 'puid')">P</a></td>
         <td class='star_directory_name'><a onPointerUp="Crux.crux.trigger('emp_dir_sort', 'name')">Name</a></td>
@@ -3840,8 +3864,8 @@ function NeptunesPrideAgent() {
         <td><a onPointerUp="Crux.crux.trigger('emp_dir_sort', 'totalStars')">Stars</a></td>
         <td>Color</td>
         <td>Shape</td>
-        </tr>`
-  
+        </tr>`;
+
         for (let empire of sortedEmpires) {
           html += `
             <tr>
@@ -3855,20 +3879,15 @@ function NeptunesPrideAgent() {
                 <a onPointerUp='Crux.crux.trigger("player_color_shape", {puid: ${empire.uid}, kind: "shape", amount: -1})' class="fontello"> &#59230;</a> </td>
   
             </tr>
-          `
+          `;
         }
-        html += "</table>"
+        html += "</table>";
       }
-  
-  
-      Crux.Text("", "rel")
-        .rawHTML(html)
-        .roost(starDir)
-  
-  
-      return starDir
-    }
 
+      Crux.Text("", "rel").rawHTML(html).roost(starDir);
+
+      return starDir;
+    };
 
     hooksLoaded = true;
   };
@@ -4006,28 +4025,30 @@ function NeptunesPrideAgent() {
   var otherUserCode: string | undefined = undefined;
   let game = getGameNumber();
   let store = new GameStore(game);
-  let superOnServerResponse = NeptunesPride.np.onServerResponse;
-  NeptunesPride.np.onServerResponse = function (
-    response: { event: string } | any[]
-  ) {
-    superOnServerResponse(response);
-    // TODO: replace this with normal event handling
-    const event = Array.isArray(response) ? response[0] : response.event;
-    if (event === "order:player_achievements") {
-      console.log("Initial load complete. Reinstall.");
-      logCount("achievements_init");
-      init();
-    } else if (event === "order:full_universe") {
-      console.log("Universe received. Reinstall.");
-      NeptunesPride.originalPlayer = NeptunesPride.universe.player.uid;
-      logCount("universe_init");
-      init();
-    } else if (!hooksLoaded && NeptunesPride.npui.map) {
-      console.log("Hooks need loading and map is ready. Reinstall.");
-      logCount(`${event}_init`);
-      init();
-    }
-  };
+  let superOnServerResponse = NeptunesPride.np?.onServerResponse;
+  if (superOnServerResponse !== undefined) {
+    NeptunesPride.np.onServerResponse = function (
+      response: { event: string } | any[]
+    ) {
+      superOnServerResponse(response);
+      // TODO: replace this with normal event handling
+      const event = Array.isArray(response) ? response[0] : response.event;
+      if (event === "order:player_achievements") {
+        console.log("Initial load complete. Reinstall.");
+        logCount("achievements_init");
+        init();
+      } else if (event === "order:full_universe") {
+        console.log("Universe received. Reinstall.");
+        NeptunesPride.originalPlayer = NeptunesPride.universe.player.uid;
+        logCount("universe_init");
+        init();
+      } else if (!hooksLoaded && NeptunesPride.npui.map) {
+        console.log("Hooks need loading and map is ready. Reinstall.");
+        logCount(`${event}_init`);
+        init();
+      }
+    };
+  }
 
   let switchUser = async function (_event?: any, data?: string) {
     if (NeptunesPride.originalPlayer === undefined) {
@@ -5452,42 +5473,76 @@ function NeptunesPrideAgent() {
     return myApiKey;
   });
 
-  restoreFromDB("game_event")
-    .then(() => updateMessageCache("game_event"))
-    .then(() => updateMessageCache("game_diplomacy"))
-    .then(() => {
-      window.setTimeout(async () => {
-        const allkeys = (await store.keys()) as string[];
-        const apiKeys = allkeys.filter((x) => x.startsWith("API:"));
-        allSeenKeys =
-          messageIndex["api"]
-            ?.flatMap((m: any) => {
-              const body = m.message.body || m.message.payload?.body;
-              return body.match(/\[\[api:\w\w\w\w\w\w\]\]/);
-            })
-            .filter((k) => k)
-            .filter((v, i, a) => a.indexOf(v) === i) || [];
-        console.log("Probable API Keys: ", allSeenKeys);
-        for (const x of apiKeys) {
-          const key = await store.get(x);
-          const check = `[[api:${key}]]`;
-          if (allSeenKeys.indexOf(check) === -1) {
-            allSeenKeys.push(check);
+  if (getGameNumber() !== undefined) {
+    restoreFromDB("game_event")
+      .then(() => updateMessageCache("game_event"))
+      .then(() => updateMessageCache("game_diplomacy"))
+      .then(() => {
+        window.setTimeout(async () => {
+          const allkeys = (await store.keys()) as string[];
+          const apiKeys = allkeys.filter((x) => x.startsWith("API:"));
+          allSeenKeys =
+            messageIndex["api"]
+              ?.flatMap((m: any) => {
+                const body = m.message.body || m.message.payload?.body;
+                return body.match(/\[\[api:\w\w\w\w\w\w\]\]/);
+              })
+              .filter((k) => k)
+              .filter((v, i, a) => a.indexOf(v) === i) || [];
+          console.log("Probable API Keys: ", allSeenKeys);
+          for (const x of apiKeys) {
+            const key = await store.get(x);
+            const check = `[[api:${key}]]`;
+            if (allSeenKeys.indexOf(check) === -1) {
+              allSeenKeys.push(check);
+            }
           }
-        }
-        console.log("Probable API Keys II: ", allSeenKeys);
-        allSeenKeys.forEach(async (key) => {
-          const code = getCodeFromApiText(key);
-          await getServerScans(code);
-          if (countScans(code) > 0) {
-            console.log(`Scans for ${code} cached`);
-            return;
-          }
-          console.log(`Scans for ${code} not cached yet, register for them`);
-          registerForScans(code);
-        });
-      }, 1000);
-    });
+          console.log("Probable API Keys II: ", allSeenKeys);
+          allSeenKeys.forEach(async (key) => {
+            const code = getCodeFromApiText(key);
+            await getServerScans(code);
+            if (countScans(code) > 0) {
+              console.log(`Scans for ${code} cached`);
+              return;
+            }
+            console.log(`Scans for ${code} not cached yet, register for them`);
+            registerForScans(code);
+          });
+        }, 1000);
+      });
+  } else {
+    const selects = document.getElementsByTagName("select");
+    if (selects.length > 1 && selects[1].innerHTML.indexOf("Players") !== -1) {
+      console.log("set up inc/dec hotkeys")
+      const setSelect = (count: number) =>  {
+        selects[1].innerHTML = "";
+        const pc = document.createElement("option");
+        pc.value = `${count}`;
+        pc.text = `${count} Players`
+        pc.setAttribute('selected', 'selected');
+        selects[1].appendChild(pc);
+        const change = new Event("change", {bubbles: true});
+        selects[1].dispatchEvent(change);
+
+      }
+      const decrementPlayerCount = () => {
+        console.log("dec")
+        setSelect(+selects[1].value - 1);
+      }
+      const incrementPlayerCount = () => {
+        console.log("inc")
+        setSelect(+selects[1].value + 1);
+      }
+      const w = window as any;
+      if (w.playerCount === undefined) {
+        defineHotkey("-", decrementPlayerCount);
+        defineHotkey("+", incrementPlayerCount);
+        w.playerCount = selects[1].value;
+      } else {
+        console.log(`loaded twice? ${w.playerCount}`)
+      }
+    }
+  }
 
   const loadScanData = () =>
     refreshScanData().then(() => {
