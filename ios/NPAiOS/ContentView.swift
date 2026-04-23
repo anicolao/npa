4120131b851cd224cf3a95ebc4b28313a51c107d
Alex Nicolaou <anicolao@gmail.com>
2026-04-23 15:38:52 -0400

    Add safe-area tap scrolling in iOS wrapper

--- ios/NPAiOS/ContentView.swift
+++ ios/NPAiOS/ContentView.swift
@@ -1,13 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var webViewController = GameWebViewController()
+
     var body: some View {
-        GameWebView()
-            .background(menuBarBackground)
+        ZStack {
+            GameWebView(controller: webViewController)
+
+            GeometryReader { geometry in
+                VStack(spacing: 0) {
+                    tapZone(height: geometry.safeAreaInsets.top) {
+                        webViewController.scrollToTop()
+                    }
+
+                    Spacer(minLength: 0)
+
+                    tapZone(height: geometry.safeAreaInsets.bottom) {
+                        webViewController.scrollToBottom()
+                    }
+                }
+                .ignoresSafeArea()
+            }
+        }
+        .background(menuBarBackground)
     }
 
     private var menuBarBackground: Color {
         Color(red: 26.0 / 255.0, green: 29.0 / 255.0, blue: 68.0 / 255.0)
+    }
+
+    private func tapZone(height: CGFloat, action: @escaping () -> Void) -> some View {
+        menuBarBackground
+            .opacity(height > 0 ? 0.001 : 0)
+            .frame(height: height)
+            .contentShape(Rectangle())
+            .onTapGesture(perform: action)
+            .allowsHitTesting(height > 0)
     }
 }
 
