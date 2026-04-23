a060eec2ab56c5d07b209f0c3c0dc61e8335d577
Alex Nicolaou <anicolao@gmail.com>
2026-04-23 16:11:42 -0400

    Use fixture origin for extension asset requests

--- src/dynamic.ts
+++ src/dynamic.ts
@@ -1,7 +1,7 @@
 const Templates = import(
-  `https://${window.location.host}/scripts/client/templates.js`
+  `${window.location.origin}/scripts/client/templates.js`
 );
-const UI = import(`https://${window.location.host}/scripts/client/widgets.js`);
+const UI = import(`${window.location.origin}/scripts/client/widgets.js`);
 
 export async function getUI() {
   return await UI;
